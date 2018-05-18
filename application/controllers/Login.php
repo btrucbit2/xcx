5d26374a369cb7c25b9a10ee7cfda9c14f31ac50
cuirj@wegene.com <cui@cuideMacBook-Pro-2.local>
2018-05-18 11:05:09 +0800

    Initial commit

---
+++ application/controllers/Login.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function index()
+	{
+        $appid = 'd761bb5a6c5d931500bf7e6b8d3d85cc
+';
+        $secret = 'wx0ca8054103d57ed4';
+        $code = $_POST['code'];
+        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.$appid.'&secret='.$secret.'&js_code='.$code.'&grant_type=authorization_code';
+        $response = file_get_contents($url);
+        return $response;
+	}
+}
