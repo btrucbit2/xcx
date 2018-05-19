3f11b4a9d138c04dc5c6518021c06d489d139e61
cuirj@wegene.com <cui@cuideMacBook-Pro-2.local>
2018-05-19 15:04:27 +0800

    ttt

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
  */
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
-echo '11111'
+echo '11111';
 exit;
 /*
  *---------------------------------------------------------------
